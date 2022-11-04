82ea2896f14b9674ad02d1752920a19754fdc273
MinseoRhie <dlalstj0213@gmail.com>
2022-11-04 08:48:54 -0700

    create project

---
+++ src/main/java/com/example/pjMgt/ProjectManagemnetApplication.java
@@ -0,0 +1,13 @@
+package com.example.pjMgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagemnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagemnetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pjMgt/ProjectManagemnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pjMgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagemnetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

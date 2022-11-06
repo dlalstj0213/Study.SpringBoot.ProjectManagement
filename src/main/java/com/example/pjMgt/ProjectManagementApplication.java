c3521181814ec36bed7da72161a32892571b6d03
MinseoRhie <dlalstj0213@gmail.com>
2022-11-06 04:27:51 -0800

    fixed typo of application class

--- src/main/java/com/example/pjMgt/ProjectManagementApplication.java
+++ src/main/java/com/example/pjMgt/ProjectManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectManagemnetApplication {
+public class ProjectManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectManagemnetApplication.class, args);
+        SpringApplication.run(ProjectManagementApplication.class, args);
     }
 
 }
--- src/main/java/com/example/pjMgt/ServletInitializer.java
+++ src/main/java/com/example/pjMgt/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ProjectManagemnetApplication.class);
+        return application.sources(ProjectManagementApplication.class);
     }
 
 }
